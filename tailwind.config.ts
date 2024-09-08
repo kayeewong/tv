a5512f53891fce3cec43a2cb1e4a66708a985a84
KayeeWong <jiayihuang0211@gmail.com>
2024-09-08 19:41:30 +0800

    feat: set theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,11 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
-  			background: 'hsl(var(--background))',
+				background: {
+          DEFAULT: 'hsl(var(--background))',
+          dark: '#3f3f3f',
+          light: '#e5e5e5',
+        },
   			foreground: 'hsl(var(--foreground))',
   			card: {
   				DEFAULT: 'hsl(var(--card))',
